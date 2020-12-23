c0f329db010e581093744d6080d8e01f379d37dc
philippetedajo <philimart21@gmail.com>
2020-12-23 18:15:52 +0100

    adding general loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import Head from "next/head";
 import "../styles/globals.scss";
+import Router from "next/router";
 import { CookiesProvider } from "react-cookie";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
 
 function MyApp({ Component, pageProps }) {
   const Template = Component.Template || EmptyTemplate;
+
+  //Binding events.
+  Router.events.on("routeChangeStart", () => NProgress.start());
+  Router.events.on("routeChangeComplete", () => NProgress.done());
+  Router.events.on("routeChangeError", () => NProgress.done());
   return (
     <>
       <CookiesProvider>
